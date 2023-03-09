2afde4255e7d6cb54b24e60fb69bc085a46fe665
user <user@192>
2023-03-09 23:14:45 +0300

    added 1st game

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 install:
 	composer install
-brain-games:
+brain-games: #start game
 	./bin/brain-games
 validate:
 	composer validate
+lint:
+	composer exec --verbose phpcs -- --standard=PSR12 src bin
+brain-even: #start even number test
+	./bin/brain-even
---
+++ src/Even.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace BrainEven\Even;
+
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
+
+use function cli\line;
+use function cli\prompt;
+
+
+function isEven($number)
+{
+    if ($number % 2 == 0) {
+        return "yes";
+    } else {
+        return "no";
+    }
+}
+
+function playEven()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line('Answer "yes" if the number is even, otherwise answer "no"');
+    for ($game = 0; $game < 3; $game++) {
+        $number = rand(0, 100);
+        line("Question: %s", $number);
+        $answer = prompt('Your answer');
+
+        if ($answer == isEven($number)) {
+            line('Correct!');
+        } else {
+            $correctAnswer = isEven($number);
+            line("'$answer' is wrong answer ;(. Correct answer was '$correctAnswer'.");
+            line("Let's try again, %s!", $name);
+            break;
+        }
+        line("Congratulations, %s!", $name);
+    }
+}
