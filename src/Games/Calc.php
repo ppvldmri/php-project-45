a06473566eacefa4506c3159b0a62f264d99f8c7
user <user@192>
2023-06-12 16:57:26 +0300

    fix 5

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -8,12 +8,12 @@ const TASK_DESCRIPTION = 'What is the result of the expression?';
 
 function playGame()
 {
-    $gameData = function () {
+    $gameData = function () {  // я не знаю, как сделать иначе. если дать фукнции имя, то у меня пропадает цикл и выражение дается одно и тоже
         $operatorArr = ["+", "-", "*"];
         $operator = $operatorArr[rand(0, 2)];
         $number1 = rand(0, 10);
         $number2 = rand(0, 10);
-        $task = "$number1 $operator $number2";
+        $task = "$number1" . " $operator " . "$number2";
         $taskAnswer = '';
         match ($operator) {
             '-' => $taskAnswer = $number1 - $number2,
@@ -23,5 +23,5 @@ function playGame()
         $gameData = [$task,$taskAnswer];
         return $gameData;
     };
-        playGameFromEngine(TASK_DESCRIPTION, $gameData);
+    playGameFromEngine(TASK_DESCRIPTION, $gameData);
 }
